a0c2be7a8dcd41ac4f70a0b83a6e40aabef2e46e
wangjinzhao <bigbaldy@163.com>
2018-07-04 20:44:20 +0800

    'init'

---
+++ src/main/java/com/qihoo/test/App.java
@@ -0,0 +1,12 @@
+package com.qihoo.test;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by wangjinzhao on 2018/7/4.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isBlank("456"));
+    }
+}
